32489358a8ac726bf1449d6883d071b7c6736bcc
Aaron Mathias <94035223+Aaronmathias61@users.noreply.github.com>
2021-12-15 13:49:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function foo(){
+    var a=document.getElementById("name").value;
+    console.log(a);
+    var b=document.getElementById("mname").value;
+    console.log(b);
+    var a=document.getElementById("lname").value;
+    console.log(a);
+    var a=document.getElementById("email").value;
+    console.log(a);
+}
